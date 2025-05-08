49b75773445a2bf4d5d8949c0710e228251fccc8
Oyabure Zakari <oyabure205@gmail.com>
2025-05-08 14:07:28 +0100

    implement leaflet map ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./Map.css";
+import L from "leaflet";
+import { useEffect, useState } from "react";
+
+// Define custom icon
+const customIcon = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png", // Path to your icon
+  iconSize: [25, 41], // Size of the icon [width, height]
+  iconAnchor: [12, 41], // Point of the icon that corresponds to marker's location [x, y]
+  popupAnchor: [1, -34], // Point where the popup opens relative to the iconAnchor [x, y]
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png", // Optional: shadow image
+  shadowSize: [41, 41], // Size of the shadow
+});
+
+const position = [51.505, -0.09];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="map-container"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position} icon={customIcon}>
+        <Popup>
+          <h2>Your Location</h2>
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
