9398cefc760ccb4b58c516db1258a583a1fe39e1
Oyabure Zakari <oyabure205@gmail.com>
2025-05-18 17:23:43 +0100

    change component name from MapUI to MapPage

---
+++ src/pages/MapPage.js
@@ -0,0 +1,22 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+import "../styles/map.css";
+
+function MapPage() {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={10}
+      scrollWheelZoom={false}
+      className="map-container"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+}
+
+export default MapPage;
